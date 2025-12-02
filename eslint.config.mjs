b226693f0c677a69b47de8066f5e3757a170eeb4
Evgeniy Valyaev <valyay@evilmartians.com>
2025-12-02 19:40:05 +0100

    feat: refactor ESLint configuration to use array format and enhance TypeScript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,27 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-export default tseslint.config(
+export default [
 	{
 		ignores: ["dist/**", "node_modules/**"],
 	},
 	eslint.configs.recommended,
 	...tseslint.configs.recommended,
 	{
+		files: ["src/**/*.ts"],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
 		linterOptions: {
 			reportUnusedDisableDirectives: false,
 		},
 		rules: {
 			"@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
 			"@typescript-eslint/explicit-function-return-type": "error",
+			"@typescript-eslint/no-misused-promises": "error",
 		},
 	},
 	{
@@ -25,4 +33,4 @@ export default tseslint.config(
 		},
 	},
 	eslintConfigPrettier,
-);
+];
